15973550e8ab70eaf5ae3e02c9641abeb1e5a3bf
Corbin Halliwill <corbin.halliwill@berkeley.edu>
2015-08-15 20:45:13 -0700

    Initial Commit

---
+++ LassyApp/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  LassyApp
+//
+//  Created by Black_Shark on 8/15/15.
+//  Copyright (c) 2015 Black_Shark. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
